49d532bea920d6effa946d53153ce8eb93c59005
Christopher R. Miller <xpm@mysteriouspants.com>
2021-02-28 00:45:55 -0800

    Use from_ne_bytes rather than custom

--- src/null_hasher.rs
+++ src/null_hasher.rs
@@ -2,19 +2,14 @@
 //! pre-hashes its keys - so hashing them again in its storage hashmap makes
 //! little sense.
 
-use std::{
-    hash::{BuildHasher, Hasher},
-};
+use std::{convert::TryInto, hash::{BuildHasher, Hasher}};
 
 /// Proxies u64's for themselves.
 pub(crate) struct NullHasher(u64);
 
 impl Hasher for NullHasher {
     fn write(&mut self, bytes: &[u8]) {
-        assert_eq!(8, bytes.len()); // only accept u64's
-        for byte in bytes.iter().rev() {
-            self.0 = (self.0 << 8) | *byte as u64;
-        }
+        self.0 = u64::from_ne_bytes(bytes.try_into().expect("Only accept u64's"));
     }
 
     fn finish(&self) -> u64 {
